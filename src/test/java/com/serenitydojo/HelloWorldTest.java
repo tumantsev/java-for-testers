4d3d4e2b82ecb21be021ae6954807558d552f0eb
Taras Umantsev <tumantsev@macbookpro.local>
2022-02-07 06:54:00 +0200

    Adding more examples

--- src/test/java/com/serenitydojo/HelloWorldTest.java
+++ src/test/java/com/serenitydojo/HelloWorldTest.java
@@ -9,4 +9,27 @@ public class HelloWorldTest {
         HelloWorldWriter write = new HelloWorldWriter();
         write.writeHelloWorld();
     }
+
+    @Test
+    public void declaringNumericalVariable() {
+
+        int age = 42;
+
+        age += 1;
+
+        System.out.println("Age = " + age);
+
+        double wight = 15.5D;
+
+        System.out.println("Weight = " + wight);
+    }
+
+    @Test
+    public void workingWithStrings() {
+
+        String firstName = "Sarah-Jane";
+        String upperCaseFirstName = firstName.toUpperCase();
+
+        System.out.println("Upper case first name " + upperCaseFirstName);
+    }
 }
