dad57dfeba7e8d5d9c97113351f1fe45ecc49d74
Taras Umantsev <umantsevtaras@gmail.com>
2022-02-11 07:25:31 +0200

    Added switch variant

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,14 +2,23 @@ package com.serenitydojo.model;
 
 public class Feeder {
 
-
     public String feeds(String animal, boolean isPremium) {
-        if (animal.equals("Cat")) {
-            return (isPremium) ? "Salmon": "Tuna";
-        } else if (animal.equals("Dog")){
-            return (isPremium) ? "Deluxe Food": "Dog Food";
-        } else {
-            return (isPremium) ? "Lettuce" : "Cabbage";
+//        if (animal.equals("Cat")) {
+//            return (isPremium) ? "Salmon": "Tuna";
+//        } else if (animal.equals("Dog")){
+//            return (isPremium) ? "Deluxe Food": "Dog Food";
+//        } else {
+//            return (isPremium) ? "Lettuce" : "Cabbage";
+//        }
+        switch (animal) {
+            case ("Cat"):
+                return (isPremium) ? "Salmon": "Tuna";
+            case ("Dog"):
+                return (isPremium) ? "Deluxe Food": "Dog Food";
+            case ("Hamster"):
+                return (isPremium) ? "Lettuce" : "Cabbage";
+            default:
+                return "Don't know the animal - don't know the food";
         }
     }
 }
